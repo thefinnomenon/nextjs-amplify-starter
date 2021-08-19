3971fe084e40cbf17f8c8206c6aaf07a7bdafd75
Chris Finn <chris@finn.com>
2021-08-19 12:53:00 -0400

    fix: typo in next/image import

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/Image';
+import Image from 'next/image';
 import { useState } from 'react';
 import styled from 'styled-components';
 import DarkModeControl from '../DarkModeControl';
